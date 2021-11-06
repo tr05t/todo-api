5348d3f028b087b43b0fff01455b363cd5ab405f
Etienne Trost <etienne.g89@gmail.com>
2021-11-06 22:08:41 +0100

    created crud methods

--- src/main/java/e2xDy/toDo/ToDoController.java
+++ src/main/java/e2xDy/toDo/ToDoController.java
@@ -2,13 +2,9 @@ package e2xDy.toDo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Controller
 @RequestMapping(path = "/api/todo")
@@ -17,19 +13,50 @@ public class ToDoController {
     @Autowired
     private ToDoRepository toDoRepository;
 
-    @GetMapping(path = "")
+    @GetMapping(path = "", name = "getToDos")
     public @ResponseBody
     Iterable<ToDo> getAllToDos() {
         return toDoRepository.findAll();
     }
 
-    @PostMapping("")
+    @GetMapping(path = "{toDoId}", name = "getToDo")
     public @ResponseBody
-    Optional<ToDo> createToDo() {
+    Optional<ToDo> getToDo(@PathVariable Integer toDoId) {
+        return toDoRepository.findById(toDoId);
+    }
+
+    @PostMapping(path = "", name = "createToDo")
+    public @ResponseBody
+    Optional<ToDo> createToDo(@RequestBody ToDoRequestModel toDoDetails) {
         ToDo toDo = new ToDo();
-        toDo.setContent(UUID.randomUUID().toString());
-        toDo.setDone(false);
+        toDo.setContent(toDoDetails.getContent());
+        toDo.setDone(toDoDetails.getDone());
         toDoRepository.save(toDo);
         return toDoRepository.findById(toDo.getId());
+    }
+
+    @PutMapping(path = "{toDoId}", name = "updateToDo")
+    public @ResponseBody
+    Optional<ToDo> updateToDo(@PathVariable Integer toDoId, @RequestBody ToDo toDoDetails) {
+        System.out.println(toDoRepository.findById(toDoId));
+        if (toDoRepository.findById(toDoId).isPresent()) {
+            ToDo toDo = toDoRepository.findById(toDoId).get();
+            toDo.setContent(toDoDetails.getContent());
+            toDo.setDone(toDoDetails.getDone());
+            toDoRepository.save(toDo);
+            return toDoRepository.findById(toDo.getId());
+        }
+        return toDoRepository.findById(toDoId);
+    }
+
+    @DeleteMapping(path = "{toDoId}", name = "deleteToDo")
+    public @ResponseBody
+    boolean deleteToDo(@PathVariable Integer toDoId) {
+        if (toDoRepository.findById(toDoId).isPresent()) {
+            ToDo toDo = toDoRepository.findById(toDoId).get();
+            toDoRepository.delete(toDo);
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/main/java/e2xDy/toDo/ToDoRequestModel.java
@@ -0,0 +1,23 @@
+package e2xDy.toDo;
+
+public class ToDoRequestModel {
+    private String content;
+
+    private Boolean done;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
