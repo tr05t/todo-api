8e29bba7ab555da44684800762e588d337a7ff2f
Etienne Trost <etienne.g89@gmail.com>
2021-11-15 15:12:41 +0100

    added dummy data and add them on container initialization

--- docker/prepare-mariadb.sql
+++ docker/prepare-mariadb.sql
@@ -7,3 +7,16 @@ CREATE TABLE to_do
     done    BIT(1)       NULL,
     CONSTRAINT pk_todo PRIMARY KEY (id)
 );
+
+CREATE TABLE hibernate_sequence
+(
+    next_val BIGINT NULL
+);
+
+INSERT INTO to_do_app.to_do (id, content, done)
+VALUES (1, 'todo_1', TRUE),
+       (2, 'todo_2', FALSE),
+       (3, 'todo_3', TRUE);
+
+INSERT INTO to_do_app.hibernate_sequence (next_val)
+VALUES (4);
