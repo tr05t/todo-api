df8a552d9ce47cc3ab383e057bf85df514b2fecc
Etienne Trost <etienne.g89@gmail.com>
2021-11-15 09:53:42 +0100

    set table name to `to_do`

--- src/main/java/com/toDo/model/ToDoModel.java
+++ src/main/java/com/toDo/model/ToDoModel.java
@@ -1,11 +1,9 @@
 package com.toDo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "to_do")
 public class ToDoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
